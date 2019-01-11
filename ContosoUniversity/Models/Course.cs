436efd5bb642be016acafa40d920306b32be2533
Nick Doran <chasedoran6@gmail.com>
2019-01-10 20:26:08 -0500

    project first build, models created

---
+++ ContosoUniversity/Models/Course.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations.Schema;
+
+
+namespace ContosoUniversity.Models
+{
+    public class Course
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.None]
+        public int CourseId { get; set; }
+        public string Title { get; set; }
+        public int Credits { get; set; }
+
+        ICollection<Enrollment> Enrollments { get; set; }
+    }
+}
